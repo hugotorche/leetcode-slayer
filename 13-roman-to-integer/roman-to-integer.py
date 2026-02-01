a24a6305edcfe45fc9a899d58dadf0f2bdc76730
hugotorche <81092822+hugotorche@users.noreply.github.com>
2026-02-01 22:48:37 +0100

    Time: 4 ms (64.35%) | Memory: 19.5 MB (10.56%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,15 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         ls = list(s)    
-        romanMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        totalInt = 0
-        for i in range(len(ls)):
-            if i > 0 and ls[i] in ['V', 'X'] and ls[i-1] == 'I':
-                totalInt += romanMap[ls[i]] - 2
-            elif i > 0 and ls[i] in ['L', 'C'] and ls[i-1] == 'X':
-                totalInt += romanMap[ls[i]] - 20
-            elif i > 0 and ls[i] in ['D', 'M'] and ls[i-1] == 'C':
-                totalInt += romanMap[ls[i]] - 200
-            else:
-                totalInt += romanMap[ls[i]]
+        romanDic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        totalInt = romanDic[ls[0]]
+        for i in range(1, len(ls)):
+            totalInt += romanDic[ls[i]]
+            if ls[i] in ['V', 'X'] and ls[i-1] == 'I':
+                totalInt += - 2
+            elif ls[i] in ['L', 'C'] and ls[i-1] == 'X':
+                totalInt += - 20
+            elif ls[i] in ['D', 'M'] and ls[i-1] == 'C':
+                totalInt += - 200
         return totalInt
