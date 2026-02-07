b732200d8e606972fce077928d4c450b34b57e40
hugotorche <81092822+hugotorche@users.noreply.github.com>
2026-02-07 17:09:21 +0100

    Time: 3 ms (62.46%) | Memory: 21.2 MB (5.96%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,4 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums_range = set(range(0, len(nums)+1))
-        return list(nums_range.difference(nums))[0]
+        return nums_range.difference(nums).pop()
