ce4940838e00faf02575399b9ab6c22a7883f15c
hugotorche <81092822+hugotorche@users.noreply.github.com>
2026-02-15 16:19:13 +0100

    Time: 1705 ms (5.26%) | Memory: 21.9 MB (57.38%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -2,9 +2,7 @@ class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         ct = 0
         s = sorted(s)
-        if not s or max(s) < min(g):
-            return ct
-        else:
+        if s and min(g) <= max(s):
             for i in range(len(g)):
                 for j in range(len(s)):
                     if s[j] >= g[i]:
