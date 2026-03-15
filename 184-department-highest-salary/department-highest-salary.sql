45fec23bd3b25e01390767f4b93a35584d434ded
hugotorche <81092822+hugotorche@users.noreply.github.com>
2026-03-15 10:33:58 +0100

    Time: 594 ms (88.46%)

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+with employee_rn as (
+    select
+        id,
+        name,
+        salary,
+        departmentid,
+        dense_rank() over (partition by departmentid order by salary desc) as rn
+    from employee
+)
+
+select
+    dep.name as department,
+    emp.name as employee,
+    emp.salary as salary
+from employee_rn as emp
+inner join department as dep
+    on emp.departmentid = dep.id
+where emp.rn = 1;
