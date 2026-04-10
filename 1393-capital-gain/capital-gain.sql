aa3566808682269c5fa458630d33501358c69fbd
hugotorche <81092822+hugotorche@users.noreply.github.com>
2026-04-10 15:01:32 +0200

    Time: 480 ms (70.10%)

---
+++ 1393-capital-gain/capital-gain.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select
+    stock_name,
+    sum(case when operation = 'Sell' then price else price * -1 end) as capital_gain_loss
+from stocks
+group by 1;
